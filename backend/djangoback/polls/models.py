1d4fdd0935f0d97941094fc1789c58f13826e00c
haeun <sj05publu@naver.com>
2020-04-02 14:50:51 +0900

    [+] model modify

--- backend/djangoback/polls/models.py
+++ backend/djangoback/polls/models.py
@@ -1,11 +1,20 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200) # charField : 문자(character), 필수 인수로 max_length가 있다.
     pub_date = models.DateTimeField('date published') # DateTimeField : 날짜와 시간 필드
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # 각각의 Choice가 하나의 Question에 관계됨
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0) # 기본 값을 0으로 설정함
+    def __str__(self):
+        return self.choice_text
